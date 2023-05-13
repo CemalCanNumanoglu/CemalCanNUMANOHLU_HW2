4d2e2267041a6c7159cef041c2c03019415d85f9
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-05-14 00:56:06 +0300

    ListViewModel created

---
+++ CemalCanNumanoglu_HW2/ViewModels/ListViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  ListViewModel.swift
+//  CemalCanNumanoglu_HW2
+//
+//  Created by Cemal can Numanoğlu on 12.05.2023.
+//
+
+import Foundation
+
+protocol ListViewModelProtocol {
+    var delegate: ListViewModelDelegate? { get set}
+    var numberOfItems: Int { get }
+    
+    func fetchData()
+    func news(_ index: Int) -> News?
+    func media(_ index: Int) -> NewsMultimedia?
+}
+
+protocol ListViewModelDelegate: AnyObject {
+    func showLoadingView()
+    func hideLoadingView()
+    func reloadData()
+}
+
+final class ListViewModel {
+    private var news: [News] = []
+    private var media: [NewsMultimedia] = []
+    let service: NYTopStoriesServiceProtocol
+    weak var delegate: ListViewModelDelegate?
+    
+    init(service: NYTopStoriesServiceProtocol, delegate: ListViewModelDelegate? = nil) {
+        self.service = service
+        self.delegate = delegate
+    }
+    
+    fileprivate func fetchNews() {
+        self.delegate?.showLoadingView()
+        service.fetchNews { [ weak self ] response in
+            guard let self else { return }
+            self.delegate?.hideLoadingView()
+            switch response {
+            case .success(let news):
+                print("News: \(news)")
+                self.news = news
+                self.media = news.compactMap { $0.newsMultimedia?.first }
+                self.delegate?.reloadData()
+            case .failure(let error):
+                print("Hata: \(error)")
+            }
+        }
+    }
+}
+
+extension ListViewModel: ListViewModelProtocol {
+    func media(_ index: Int) -> NewsMultimedia? {
+        media[index]
+    }
+    
+    var numberOfItems: Int {
+        news.count
+    }
+    
+    func fetchData() {
+        fetchNews()
+    }
+    
+    func news(_ index: Int) -> News? {
+        news[index]
+    }
+    
+    
+}
