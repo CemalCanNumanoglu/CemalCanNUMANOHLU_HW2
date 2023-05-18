2647f83467fe15b17502f7423280bdb6dea1bfa5
Cemal Can NUMANOĞLU <117599373+CemalCanNumanoglu@users.noreply.github.com>
2023-05-19 02:01:22 +0300

    data transfer fail,fixed

--- CemalCanNumanoglu_HW2/ViewControllers/ListViewController.swift
+++ CemalCanNumanoglu_HW2/ViewControllers/ListViewController.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+//MARK: ListViewController
+class ListViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
@@ -20,18 +20,21 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         listViewModel.fetchData()
+        self.tabBarController?.navigationItem.title = "News"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "News"
         tableView.register(cellType: ListTableViewCell.self)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
-
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
+//MARK: - Extensions
+extension ListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         listViewModel.numberOfItems
     }
@@ -41,42 +44,43 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         if let news = self.listViewModel.news(indexPath.row){
             cell.configure(news: news)
         }
-        if let media = self.listViewModel.media(indexPath.row) {
+        if let media = self.listViewModel.smallmedia(indexPath.row) {
             cell.configureMedia(media: media)
         }
         
-        cell.layer.cornerRadius = 30
-        cell.layer.borderColor = UIColor.black.cgColor
+        cell.newsImageView.layer.cornerRadius = 40
+        cell.newsImageView.layer.borderColor = UIColor.black.cgColor
+        cell.newsImageView.layer.borderWidth = 1
         cell.layer.borderWidth = 1
-        cell.layoutMargins = UIEdgeInsets(top: .zero, left: 30, bottom: 10, right: 30)
+        cell.layer.borderColor = UIColor.black.cgColor
         return cell
     }
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //TODO: Details view' e data aktar
+        
+        guard let news = listViewModel.news(indexPath.row),
+              let media = listViewModel.largemedia(indexPath.row) else {return}
+
+        let detailViewModel = DetailsViewModel(news: news, media: media)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let detailVC = storyboard.instantiateViewController(identifier: "DetailsViewController") as! DetailsViewController
+        detailVC.detailsViewModel = detailViewModel
+        self.present(detailVC, animated: true)
+        
+
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        250
-    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { 180 }
     
 }
 
 
-extension ViewController: ListViewModelDelegate, LoadingShowable {
-    func showLoadingView() {
-        showLoading()
-    }
+extension ListViewController: ListViewModelDelegate, LoadingShowable {
+    func showLoadingView() { showLoading() }
     
-    func hideLoadingView() {
-        hideLoading()
-    }
+    func hideLoadingView() { hideLoading() }
     
-    func reloadData() {
-        tableView.reloadData()
-    }
-    
-    
+    func reloadData() { tableView.reloadData() }
 }
 
